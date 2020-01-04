163dbb010985fb9b5b312832990cf90dba84349e
William Busby <william.busby3435@gmail.com>
2020-01-04 11:28:18 -0700

    added HATEOAS

--- src/main/java/io/javabrains/messenger/models/Message.java
+++ src/main/java/io/javabrains/messenger/models/Message.java
@@ -1,7 +1,9 @@
 package io.javabrains.messenger.models;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.bind.annotation.XmlRootElement;
@@ -15,6 +17,7 @@ public class Message {
 	private Date created;
 	private String author;
 	private Map<Long, Comment> comments = new HashMap<>();
+	private List<Link> links = new ArrayList<>();
 	
 	public Message() {
 	}
@@ -59,5 +62,16 @@ public class Message {
 		this.comments = comments;
 	}
 
-	
+	public List<Link> getLinks() {
+		return links;
+	}
+
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+
+	public void addLink(String url, String rel) {
+		Link link = new Link(url, rel);
+		links.add(link);
+	}
 }
